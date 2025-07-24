603af849abf994faa78c4aa8d2e2df2282c86e50
prosystem-pro <prosystem490@gmail.com>
2025-07-24 12:03:26 -0600

    Cambiado a railway

--- src/app/Entornos/Entorno.prod.ts
+++ src/app/Entornos/Entorno.prod.ts
@@ -1,6 +1,6 @@
 export const Entorno = {
   Produccion: true,
-  ApiUrl: 'https://carritoweb-promesadedios-api.onrender.com/api/',
+  ApiUrl: 'https://carritoweb-promesadedios-api-production.up.railway.app/api/',
   // ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
   NombreEmpresa: 'PromesaDeDios'
 };
